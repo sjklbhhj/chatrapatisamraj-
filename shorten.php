15e10a4741934c163465e122db33f5e9190d02b0
Chatrapati <146621480+sjklbhhj@users.noreply.github.com>
2023-10-01 20:34:07 +0530

    Update shorten.php

--- shorten.php
+++ shorten.php
@@ -4,7 +4,7 @@ if (isset($_POST['original_url'])) {
     // You should implement actual URL shortening logic here and store it in a database.
     // For this example, we'll generate a random short code.
     $short_code = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 6);
-    $shortened_url = "http://yourdomain.com/$short_code";
+    $shortened_url = "https://sjklbhhj.github.io/chatrapatisamraj-//$short_code";
 
     // Redirect back to the index page with the shortened URL as a query parameter.
     header("Location: index.php?shortened=$shortened_url");
