f8617ee33f9b5a3e04efd7a1e8e2a6245c0ec039
Chatrapati <146621480+sjklbhhj@users.noreply.github.com>
2023-10-01 20:28:45 +0530

    To make a website

---
+++ shorten.php
@@ -0,0 +1,13 @@
+<?php
+if (isset($_POST['original_url'])) {
+    $original_url = $_POST['original_url'];
+    // You should implement actual URL shortening logic here and store it in a database.
+    // For this example, we'll generate a random short code.
+    $short_code = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 6);
+    $shortened_url = "http://yourdomain.com/$short_code";
+
+    // Redirect back to the index page with the shortened URL as a query parameter.
+    header("Location: index.php?shortened=$shortened_url");
+    exit();
+}
+?>
